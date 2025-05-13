cd51af0f230ceffc906fe8e601374be277c0a09a
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2025-05-13 19:33:21 -0400

    adding pkg json and express modules dependencies, adding git ignore

---
+++ backend_gf_fundamusical/app.mjs
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Pendiente para coninuar, no se que porongas se hace aqui, pero bueno, voy a ver de que se trata todo desde el node antes de usar el framework. 
