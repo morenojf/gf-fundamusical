c1a963f8a7b27d14e2ad79bd913543cffeb44796
morenojf <fjmoreno2101@gmail.com>
2025-05-22 13:34:53 -0400

    adding MySQL DB connection and changing Model Class name for being generic

--- backend_gf_fundamusical/model/localdb/localdb.mjs
+++ backend_gf_fundamusical/model/localdb/localdb.mjs
@@ -4,19 +4,21 @@ const require = createRequire(import.meta.url)
 export const readJSON = (path) => require(path)
 
 // import { readJSON } from '../../utils.mjs' // importacion de funcion para leer el json como DB local
-const moviesLocalDB = readJSON('../localdb/example.json') // importo la base de datos desde el archivo example.json con la funcion readJSON.
+const moviesDB = readJSON('../localdb/example.json') // importo la base de datos desde el archivo example.json con la funcion readJSON.
 
-export class modelLocalDB {
+export class modelDB {
   static getAll () {
-    const movies = moviesLocalDB
+    const movies = moviesDB
     return movies
   }
 
   static getById (id) {
-    const movies = moviesLocalDB
+    const movies = moviesDB
     const movie = movies.find((movie) => movie.imdbID === id)
     return movie
   }
 }
 
 // la funcion getAll devuelve todas las peliculas de la base de datos local
+
+// IMPORTANTE: Idealmente la calse se llama ModelDB para que sea mas generico y poder usarla importandola pero cambiando el src y no el nombre de las llamadas de funcion
--- backend_gf_fundamusical/model/mysql-db/dbfundamusical.mjs
+++ backend_gf_fundamusical/model/mysql-db/dbfundamusical.mjs
@@ -1,19 +1,22 @@
 // Este archivo importa, inicializa y exporta el model de la base de datos mysql
 
-// import mysql from 'mysql2/promise'
+import mysql from 'mysql2/promise'
 
-// const config = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'fundamusicaldb',
-//   port: 3306
-// }
+const config = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'fundamusicaldb',
+  port: 3306
+}
 
-// const connection = await mysql.createConnection(config)
+const connection = mysql.createConnection(config) // esto crea la conexion a la base de datos mysql y ya podemos usarla para diversas cosas como por ejemplo connection.query para hacer consultas a la base de datos.
 
-// export class fundamusicaldb {
-//   if (connection) {
-//     return connection
-//   }
-// }
+export class modelDB {
+  static async getAll () {
+    const result = await connection.query('SELECT * FROM movies')
+    return result
+  }
+}
+
+// Idealmente trabajar esto junto con la base de datos local para poder testear
