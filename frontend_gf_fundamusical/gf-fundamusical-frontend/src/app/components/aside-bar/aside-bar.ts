09885a3696dd92db6d457937fe36607177ca25c2
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2025-06-03 22:08:33 -0400

    adding routerLink on nav bar for dashboard and vista-gestion, working, also adding variable for getting current year for further usage on dropdown acordion functionality

--- frontend_gf_fundamusical/gf-fundamusical-frontend/src/app/components/aside-bar/aside-bar.ts
+++ frontend_gf_fundamusical/gf-fundamusical-frontend/src/app/components/aside-bar/aside-bar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-aside-bar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './aside-bar.html',
   styleUrl: './aside-bar.css'
 })
--- frontend_gf_fundamusical/gf-fundamusical-frontend/src/app/components/dropdown-pi-periodo/dropdown-pi-periodo.ts
+++ frontend_gf_fundamusical/gf-fundamusical-frontend/src/app/components/dropdown-pi-periodo/dropdown-pi-periodo.ts
@@ -4,18 +4,19 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-dropdown-pi-periodo',
   imports: [],
   templateUrl: './dropdown-pi-periodo.html',
-  styleUrl: './dropdown-pi-periodo.css'
+  styleUrl: './dropdown-pi-periodo.css',
 })
 export class DropdownPIPeriodo implements OnInit {
-
   // Variable para controlar qué tarjeta del acordeón está activa
   activeAccordion: string | null = '2025'; // '2025' para que inicie abierta, o null para iniciar todos cerrados
 
-  constructor() { }
+  currentYear: number;
 
-  ngOnInit(): void {
-    // Aquí puedes inicializar datos o lógica cuando el componente se carga
+  constructor() {
+    this.currentYear = new Date().getFullYear();
   }
+
+  ngOnInit(): void {}
 
   // Método para alternar la visibilidad del contenido del acordeón
   toggleAccordion(year: string): void {
