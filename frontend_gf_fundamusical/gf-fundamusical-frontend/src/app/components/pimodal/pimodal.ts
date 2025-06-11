6913fc960710b930d300e7ea6412c6b7afe067a1
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2025-06-10 21:32:21 -0400

    modal form working properly, pending creating a new PI along new period and sending form information to backend for designing the new PI

--- frontend_gf_fundamusical/gf-fundamusical-frontend/src/app/components/pimodal/pimodal.ts
+++ frontend_gf_fundamusical/gf-fundamusical-frontend/src/app/components/pimodal/pimodal.ts
@@ -1,130 +1,165 @@
 import {
-	ChangeDetectorRef,
-	Component,
-	inject,
-	OnInit,
-	TemplateRef,
-	viewChild,
-	ViewContainerRef,
-	NgModule
+  ChangeDetectorRef,
+  Component,
+  inject,
+  OnInit,
+  TemplateRef,
+  viewChild,
+  ViewContainerRef,
+  NgModule,
 } from '@angular/core';
 import { ModalService } from '../../../services/modal/modal-service';
 import { closeModalDirective } from '../../DIrectives/close-modal.directive';
-import { ReactiveFormsModule, FormControl, FormGroup, FormsModule } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  FormsModule,
+} from '@angular/forms';
 import PiObject from '../../Models/PiObject';
 import { PiService } from '../../../services/pi-design/pi-service';
+import { find } from 'rxjs';
+import SubCategoria from '../../Models/subCategorias';
+import PlanCuenta from '../../Models/PCModel';
 
 @Component({
-	selector: 'app-pimodal',
-	imports: [closeModalDirective, ReactiveFormsModule, FormsModule],
-	templateUrl: './pimodal.html',
-	styleUrl: './pimodal.css',
+  selector: 'app-pimodal',
+  imports: [closeModalDirective, ReactiveFormsModule, FormsModule],
+  templateUrl: './pimodal.html',
+  styleUrl: './pimodal.css',
 })
 export class PImodal implements OnInit {
-	
-	id!: number;
-	
-	// Declaracion variable grupoFormulario SC
-	public formSC!: FormGroup;
-	// Campo del formulario
-	public selectedSC!: FormControl;
-	// Declaracion array para guardar los id de SC
-	public scValues: number[] = []; 
+  id!: number;
 
-	// Declaración variable grupoFormulario planRow
-	public formPR!: FormGroup;
-	// Campo formulario plan Cuenta
-	public selectedPC!: FormControl;
-	// Objeto a pushear en el array final
-	public planRowObject: object = {}
+  // Declaracion variable grupoFormulario SC
+  public formSC!: FormGroup;
+  // Campo del formulario
+  public selectedSC!: FormControl;
+  // Declaracion array para guardar los id de SC
+  public scValues: number[] = [];
 
-	// Array de objetos para fetch al backend
-	public designedPI: any[];
+  // Declaración variable grupoFormulario planRow
+  public formPR!: FormGroup;
+  // Campo formulario plan Cuenta
+  public selectedPC!: FormControl;
+  // Declaracion array para guardar los id de PC seleccionados
+  public pcValues: number[] = [];
+  // Objeto a pushear en el array final
+  public planRowObject: object = {};
 
-	// SubFields
-	 
+  // Array de objetos para fetch al backend
+  public designedPI: any[];
 
-	
-	// CONSTRUCTOR -----------------------------------------------------------
+  // Subcatefields
+  subcatName!: any[];
+
+  // planCuentaFields
+  planRowNames!: any[];
+
+  // CONSTRUCTOR -----------------------------------------------------------
 
   constructor(public servicePi: PiService, private cdr: ChangeDetectorRef) {
-	  // planInversion ID por defecto para encontrar subcategorias
-	  this.id = 1
+    // planInversion ID por defecto para encontrar subcategorias
+    this.id = 1;
 
-	  this.designedPI = [];
+    this.designedPI = [];
 
+    this.subcatName = [];
+
+    this.planRowNames = [];
   }
-
 
   // ON INIT ---------------------------------------------------------------
   ngOnInit(): void {
-	this.selectedSC = new FormControl()
-	this.selectedPC = new FormControl()
+    this.selectedSC = new FormControl();
+    this.selectedPC = new FormControl();
 
-	this.formSC = new FormGroup({
-		selectedId: this.selectedSC
-	});
+    this.formSC = new FormGroup({
+      selectedId: this.selectedSC,
+    });
 
-	this.formPR = new FormGroup({
-		selectedPC: this.selectedPC
-	});
-
+    this.formPR = new FormGroup({
+      selectedPC: this.selectedPC,
+    });
   }
 
-  genTrigger(){
-	this.getSC();
-	this.getPC();
-	this.openDialog();
+  genTrigger() {
+    this.getSC();
+    this.getPC();
+    this.openDialog();
 
-	// Inicializar el formulario de SC con trigger --------
+    // Inicializar el formulario de SC con trigger --------
+  }
+  // FILTRADO DE SUBCATEGORIAS SEGUN SELECCION
 
+  get filteredSubCategorias(): SubCategoria[] {
+    if (!this.servicePi.subCategorias) return [];
+    return this.servicePi.subCategorias.filter(
+      (sc) => !this.scValues.includes(sc.subcategoriaId)
+    );
+  }
 
+  // FILTRADO DE PLANES DE CUENTA SEGUN SELECIONADOS
+
+  get filteredPC(): PlanCuenta[] {
+    if (!this.servicePi.planesCuenta) return [];
+    return this.servicePi.planesCuenta.filter(
+      (sc) => !this.pcValues.includes(sc.planCuentaId)
+    );
   }
 
   onPlanCuentaChange(event: Event) {
-  const selectElement = event.target as HTMLSelectElement;
-  const selectedId = Number(selectElement.value);
-  this.id = selectedId; // Cambias la variable por el valor seleccionado
-  this.getSC(); // Llamas a la función que hace la petición
-}
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedId = Number(selectElement.value);
+    this.id = selectedId; // Cambias la variable por el valor seleccionado
+    this.getSC(); // Llamas a la función que hace la petición
+    // Reinicias los arrays de subcategorias para que no se mezclen
+    this.subcatName = [];
+    this.scValues = [];
+  }
 
-// OBTENER SUBCARTEGORIAS SEGUN PC ID SELECTED
-getSC() {
-this.servicePi.getInfoSC(this.id).subscribe({
-  next: (data) => {
-	this.servicePi.subCategorias = data
-	this.cdr.detectChanges();
-	// Asigna el primer valor solo si aún no hay uno seleccionado
-    if (data.length > 0) {
-        this.selectedSC.setValue(data[0].subcategoriaId);
-      } else {
-		this.selectedSC.setValue(null);
-	  }
-},
-  error: (e) => {
-	if (e){	
-		console.log('No existen Subcategorias para este plan de inversión: error: ',e);
-		this.servicePi.subCategorias = null;
-		// Se hace un array vacio para mostrarlo en el html
-		this.selectedSC.setValue(null);
-	}
-  },
-});
-}
+  // OBTENER SUBCARTEGORIAS SEGUN PC ID SELECTED
+  getSC() {
+    this.servicePi.getInfoSC(this.id).subscribe({
+      next: (data) => {
+        this.servicePi.subCategorias = data;
+        this.cdr.detectChanges();
+        // Asigna el primer valor solo si aún no hay uno seleccionado
+        if (data.length > 0) {
+          this.selectedSC.setValue(data[0].subcategoriaId);
+        } else {
+          this.selectedSC.setValue(null);
+        }
+      },
+      error: (e) => {
+        if (e) {
+          console.log(
+            'No existen Subcategorias para este plan de inversión: error: ',
+            e
+          );
+          this.servicePi.subCategorias = null;
+          // Se hace un array vacio para mostrarlo en el html
+          this.selectedSC.setValue(null);
+        }
+      },
+    });
+  }
 
-// OBTENER PLANES DE CUENTA DISPONIBLES
+  // OBTENER PLANES DE CUENTA DISPONIBLES
 
   getPC() {
-  this.servicePi.getInfoPC().subscribe({
-    next: (data) => {
-      this.servicePi.planesCuenta = data;
-      // Asigna el primer valor solo si aún no hay uno seleccionado
-      if (!this.selectedPC.value && data.length > 0) {
-        this.selectedPC.setValue(data[0].planCuentaId);
-      }
-    },
-    error: (e) => { console.log(e); }
-  });
+    this.servicePi.getInfoPC().subscribe({
+      next: (data) => {
+        this.servicePi.planesCuenta = data;
+        // Asigna el primer valor solo si aún no hay uno seleccionado
+        if (!this.selectedPC.value && data.length > 0) {
+          this.selectedPC.setValue(data[0].planCuentaId);
+        }
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
   }
   // --------------------------------------------------------------------
 
@@ -144,33 +179,72 @@ this.servicePi.getInfoSC(this.id).subscribe({
 
   // FORM DATA ----------------------------------------------------------
 
-
   addSC(): void {
-	if (this.formSC.value.selectedId === null) {
-		console.log('Aqui no pasarán valores nulos, pendejo')
-		return
-	} else {
-		console.log('este es el que se pushea', this.formSC.value)
-		this.scValues.push(this.formSC.value.selectedId)
-		console.log('esto es lo que tienes dentro del array', this.scValues)
-	}
-  };
+    if (this.formSC.value.selectedId === null) {
+      console.log('Aqui no pasarán valores nulos, pendejo');
+      return;
+    } else {
+      console.log('este es el que se pushea', this.formSC.value);
+      this.scValues.push(Number(this.formSC.value.selectedId));
+      console.log('esto es lo que tienes dentro del array', this.scValues);
 
-  addPR(): void {
-	console.log('llegué hasta aqui y tome el siguiente valor del formulario para el plan de cuentas', this.formPR.value)
-	this.planRowObject = {planCuentaId: this.formPR.value.selectedPC, subcategorias: this.scValues}
-	
-	console.log('asi es como va mi ojeto', this.planRowObject)
-	
-	this.designedPI.push(this.planRowObject)
-	console.log('asi es como va mi array de objetos para el post', this.designedPI)
-
-	this.scValues = []
-	return
+      // guardar el nombre de las subcategorias seleccionadas en base a los id porque antes solo se guardaban los id sin nombre
+      const findName = this.servicePi.subCategorias?.find(
+        (sc) => sc.subcategoriaId == this.formSC.value.selectedId
+      )?.subcategoriaName;
+      console.log(findName, 'esto hago con el find');
+      // se pushea a un array de string para almacenar e iterar y mostrar los valores seleccionados en el template
+      this.subcatName.push(findName);
+    }
   }
 
+  addPR(): void {
+    console.log(
+      'llegué hasta aqui y tome el siguiente valor del formulario para el plan de cuentas',
+      this.formPR.value
+    );
+    this.pcValues.push(Number(this.formPR.value.selectedPC));
+    this.planRowObject = {
+      planCuentaId: Number(this.formPR.value.selectedPC),
+      subcategorias: this.scValues,
+    };
 
+    console.log('asi es como va mi ojeto', this.planRowObject);
+
+    this.designedPI.push(this.planRowObject);
+    console.log(
+      'asi es como va mi array de objetos para el post',
+      this.designedPI
+    );
+
+    // Encntrar nombre plan de cuenta
+    const findName = this.servicePi.planesCuenta?.find(
+      (pc) => pc.planCuentaId == this.formPR.value.selectedPC
+    )?.planCuentaName;
+    const findCode = this.servicePi.planesCuenta?.find(
+      (pc) => pc.planCuentaId == this.formPR.value.selectedPC
+    )?.planCuentaCode;
+
+    if (!this.subcatName.length) {
+      const prUserObject = {
+        nombrePlan: findName,
+        codigoPlan: findCode,
+        attachedSC: 'No se seleccionaron subcategorias para este elemento',
+      };
+      this.planRowNames.push(prUserObject);
+    } else {
+      const prUserObject = {
+        nombrePlan: findName,
+        codigoPlan: findCode,
+        attachedSC: this.subcatName.join(', '),
+      };
+      this.planRowNames.push(prUserObject);
+    }
+
+    this.scValues = [];
+    this.subcatName = [];
+    return;
+  }
 
   // --------------------------------------------------------------------
-
 }
--- frontend_gf_fundamusical/gf-fundamusical-frontend/src/services/pi-design/pi-service.ts
+++ frontend_gf_fundamusical/gf-fundamusical-frontend/src/services/pi-design/pi-service.ts
@@ -9,10 +9,13 @@ import SubCategoria from '../../app/Models/subCategorias';
 })
 export class PiService {
   readonly SC_URL =
-    'http://localhost:3128/api/gestion/modal/plancuenta-subcategoria';
+    'http://localhost:3128/api/gestion/modal/plancuenta-subcategoria'; //OBTIENE SC EN BASE A ID PLANES DE CUENTA
   readonly API_URL = 'http://localhost:3128/api/gestion-modal'; // OBTIENE TODOS LOS PLANES DE CUENTA DISPONIBLES PARA SELECCIONAR
+  
+  readonly CREATEPI_URL = 'http://localhost:3128/api/gestion-modal'
   planesCuenta!: PlanCuenta[];
   subCategorias!: SubCategoria[] | null;
+  newPI!: any[];
 
   constructor(private http: HttpClient) {}
 
@@ -22,5 +25,9 @@ export class PiService {
 
   getInfoSC(id: number) {
     return this.http.get<SubCategoria[]>(`${this.SC_URL}/${id}`);
+  }
+
+  newPeriodPI(){
+	return this.http.post<any[]>(this.CREATEPI_URL);
   }
 }
